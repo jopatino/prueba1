6f7265287e24b6147397b71a41f076c34089df9a
jopatino <36779409+jopatino@users.noreply.github.com>
2018-05-05 08:59:31 -0500

    1

---
+++ prueba1/prueba1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using prueba1.Models;
+
+namespace prueba1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
